da96836ea4481ac2665e2aac7c8e80f685530f5c
FSWL <fswl98@gmail.com>
2019-07-28 16:18:21 +0700

    Added structure and vuex store

---
+++ src/store/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    todos: []
+  },
+  getters: {
+    TODOS: state => {
+      return state.todos
+    },
+    DONE_TODOS: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    UNDONE_TODOS: state => {
+      return  state.todos.filter(todo => todo.done === false)
+    }
+  },
+  mutations: {
+    ADD_TODO: (state, payload) => {
+      state.todos.push(payload)
+    },
+    DELETE_TODO: (state, payload) => {
+      state.todos.slice(payload.id, 1)
+    }
+  },
+  actions: {
+    addToDo({commit}, todo) {
+      commit('ADD_TODO', todo)
+    },
+    deleteToDo({commit}, todo) {
+      commit('DELETE_TODO', todo)
+    }
+  },
+});
